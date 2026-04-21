4df4a4755397410c114829b8eb40070b399d0aac
jerome3590 <jerome.dixon90@gmail.com>
2026-04-21 08:26:32 +0000

    chore: untrack archive/ folder and gitignore it

--- archive/emr/boostrap/emr_gpu_bootstrap.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -x -e
-
-sudo mkdir -p /spark-rapids-cgroup/devices
-sudo mount -t cgroup -o devices cgroupv1-devices /spark-rapids-cgroup/devices
-sudo chmod a+rwx -R /spark-rapids-cgroup
-
-sudo pip install numpy requests networkx boto3 pandas matplotlib
